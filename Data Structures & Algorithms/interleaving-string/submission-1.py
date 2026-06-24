d27fda195ee3e48c247b5e76cfbcdcfb4f3067b9
Prashant Pathak <55931362+prashant2-4-4@users.noreply.github.com>
2026-06-24 14:52:48 +0530

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        memo = {}
+        def recursion(i , j):
+            if i == len(s1) and j == len(s2):
+                return True
+            if (i,j) in memo:
+                return memo[(i , j)]
+            
+            if i < len(s1) and s1[i] == s3[i+j] and recursion(i+1 , j):
+                return True
+            
+            if j < len(s2) and s2[j] == s3[i+j] and recursion(i , j+1):
+                return True
+            
+            memo[(i,j)] = False
+            return False
+        
+        return recursion(0 , 0)
+        
