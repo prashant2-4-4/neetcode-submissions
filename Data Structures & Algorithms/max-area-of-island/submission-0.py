480aa26ae8a071d6696bc7271a36dced22629bb8
Prashant Pathak <55931362+prashant2-4-4@users.noreply.github.com>
2026-07-01 10:18:21 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        directions = [[-1 , 0] , [1,0] , [0 , -1] , [0 , 1]]
+        visited = set()
+        max_count = 0
+        for i in range(n):
+            for j in range(m):
+                if (i,j) not in visited and grid[i][j] == 1:
+                    # print((i,j))
+                    count = 0
+                    queue = deque()
+                    queue.append((i,j))
+                    visited.add((i,j))
+                    while queue:
+                        nx , ny = queue.popleft()
+                        count += 1
+                        
+                        for dx , dy in directions:
+                            n_nx = nx+ dx
+                            n_ny = ny + dy
+                            # print(n_nx , n_ny , grid[n_nx][n_ny] , visited)
+                            if 0 <= n_nx < n and 0 <= n_ny < m and grid[n_nx][n_ny] == 1 and (n_nx , n_ny) not in visited:
+                                # print('here' , (n_nx , n_ny))
+                                queue.append((n_nx , n_ny))
+                                visited.add((n_nx , n_ny))
+                    max_count = max(max_count , count)
+
+        return max_count 
+
+
+
+
+        
+        
