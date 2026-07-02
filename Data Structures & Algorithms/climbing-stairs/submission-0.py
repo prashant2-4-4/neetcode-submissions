03df1543cb740d9672ec92822254a2280610908a
Prashant Pathak <55931362+prashant2-4-4@users.noreply.github.com>
2026-07-02 22:32:30 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,11 @@
+# recursion 2**n
+# sub problem solving many time so memo O(n)
+# dp problem , fibonnanci number
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1]*(n+1)
+
+        for i in range(n-2 , -1 , -1):
+            dp[i] = dp[i+1] + dp[i+2]
+        
+        return dp[0]
