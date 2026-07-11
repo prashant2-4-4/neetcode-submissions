c616aa326e240a6195f6e41a79ff86cba107d915
Prashant Pathak <55931362+prashant2-4-4@users.noreply.github.com>
2026-07-11 18:19:16 +0530

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.py
@@ -0,0 +1,25 @@
+# sudo polynomial , O(n * len(sum))
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
+            return False
+        n = len(nums)
+        memo = {}
+        def dfs(cum , idx):
+            if (2 * cum) == total_sum:
+                return True
+            if idx == n:
+                return False
+            if (cum , idx) in memo:
+                return memo[(cum,idx)]
+
+            if dfs(cum + nums[idx] , idx+1):
+                return True
+            if dfs(cum , idx+1):
+                return True
+            
+            memo[(cum,idx)] = False
+            return memo[(cum,idx)]
+        
+        return dfs(0 , 0)
