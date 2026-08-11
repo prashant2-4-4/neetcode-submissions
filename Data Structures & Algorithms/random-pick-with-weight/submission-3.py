8febf7d794201e37923d4ca460221c4246315a00
Prashant Pathak <55931362+prashant2-4-4@users.noreply.github.com>
2026-08-11 15:58:01 +0530

    Add: random-pick-with-weight - submission-3

---
+++ Data Structures & Algorithms/random-pick-with-weight/submission-3.py
@@ -0,0 +1,32 @@
+
+class Solution:
+    def __init__(self, w: List[int]):
+        if w:
+            self.prefix_sum = [w[0]]
+            for we in w[1:]:
+                last_prefix = self.prefix_sum[-1]
+                self.prefix_sum.append(we + last_prefix)
+        
+
+    def pickIndex(self) -> int:
+    
+        number = random.randint(1 ,self.prefix_sum[-1])
+
+        lo , hi = 0 , len(self.prefix_sum)-1
+        while lo < hi:
+            mid = (lo+hi)//2
+
+            # mid = (lo + hi) >> 1
+
+            if self.prefix_sum[mid] < number:
+                lo = mid + 1
+            else:
+                hi = mid
+        
+        return lo
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
