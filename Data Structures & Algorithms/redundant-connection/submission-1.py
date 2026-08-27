4bea0ff30bb3d6abee64984dd8312dff035b4e01
Prashant Pathak <55931362+prashant2-4-4@users.noreply.github.com>
2026-08-27 15:49:09 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        size = [1] * (n+1)
+        parent = [i for i in range(n+1)]
+
+        # finding group in which it belongs
+        def find(x):
+            if parent[x] == x:
+                return x
+            
+            parent[x] = find(parent[x])
+            return parent[x]
+        
+        
+        def union(n1 , n2):
+            p1 , p2 = find(n1) , find(n2)
+            #belonging same group , adding them will cause cycle
+            if p1 == p2:
+                return False
+            
+            if size[p1] > size[p2]:
+                parent[p1] = parent[p2]
+                size[p1] += size[p2]
+            
+            else:
+                parent[p2] = parent[p1]
+                size[p2] += size[p1]
+
+            return True
+
+
+        for n1 , n2 in edges:
+            if not union(n1,n2):
+                return [n1,n2]
+        
